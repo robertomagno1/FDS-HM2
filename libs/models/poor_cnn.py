bc31655821fc8bcc79a0941868cd4df9bcc0e5e8
Roberto Magno Mazzotta <150812681+robertomagno1@users.noreply.github.com>
2024-11-24 15:42:38 +0100

    Add files via upload

--- libs/models/poor_cnn.py
+++ libs/models/poor_cnn.py
@@ -1,24 +1,25 @@
-import torch.nn as nn
 import torch
 
-class PoorPerformingCNN(nn.Module):
+import torch.nn as nn
+
+class PoorPerformingCNN(nn.Module):  # Correct inheritance from nn.Module
     def __init__(self):
-        super(PoorPerformingCNN, self).__init__()
-        ##############################
-        ###     CHANGE THIS CODE   ###
-        ##############################  
+        super(PoorPerformingCNN, self).__init__()  # Explicitly call the parent class constructor
+
+        # Define the layers
         self.conv1 = nn.Conv2d(3, 4, kernel_size=3, stride=1, padding=1)
         self.relu1 = nn.ReLU()
-        self.pool = nn.MaxPool2d(kernel_size=2, stride=2, padding=0)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.conv2 = nn.Conv2d(5, 8, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(4, 8, kernel_size=3, stride=1, padding=1)
         self.relu2 = nn.ReLU()
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
 
-        self.fc1 = nn.Linear(8 * 4 * 4, 28)
+        self.fc1 = nn.Linear(8 * 8 * 8, 10)  # Flattened output size from conv layers
 
     def forward(self, x):
-        x = self.pool(self.relu1(self.conv1(x)))
-        x = self.pool(self.relu2(self.conv2(x)))
-        x = x.view(-1, 8 * 8 * 8)
+        x = self.pool1(self.relu1(self.conv1(x)))
+        x = self.pool2(self.relu2(self.conv2(x)))
+        x = x.view(x.size(0), -1)  # Flatten for fully connected
         x = self.fc1(x)
         return x
