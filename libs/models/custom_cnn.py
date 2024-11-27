6d1b21180e38ff9a8f8c538a9dab84b2f6a54d1f
Roberto Magno Mazzotta <150812681+robertomagno1@users.noreply.github.com>
2024-11-27 22:24:01 +0100

    Add files via upload

--- libs/models/custom_cnn.py
+++ libs/models/custom_cnn.py
@@ -1,20 +1,53 @@
 import torch
 from torch import nn
-from torch.nn import functional as F
 
 class CustomCNN(nn.Module):
-    def __init__(self):
-        super(CustomCNN, self).__init__()
-        ##############################
-        ###     YOUR CODE HERE     ###
-        ##############################  
-        # Define the convolutional layers
+    def __init__(self, num_classes=10, input_size=32):
+        super().__init__()  # Correct usage of super()
         
-        pass
+        # Convolutional blocks
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1)
+        self.bn1 = nn.BatchNorm2d(16)
+        self.relu1 = nn.ReLU(inplace=True)
+        self.pool1 = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(32)
+        self.relu2 = nn.ReLU(inplace=True)
+        self.pool2 = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.bn3 = nn.BatchNorm2d(64)
+        self.relu3 = nn.ReLU(inplace=True)
+        self.pool3 = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+        # Global average pooling to make the model flexible to input size
+        self.global_pool = nn.AdaptiveAvgPool2d((1, 1))
+        
+        # Fully connected layers
+        self.fc1 = nn.Linear(64, 256)
+        self.fc1_relu = nn.ReLU(inplace=True)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc2_relu = nn.ReLU(inplace=True)
+        self.fc3 = nn.Linear(128, num_classes)
+        
+        # Dropout for regularization
+        self.dropout = nn.Dropout(0.5)
 
     def forward(self, x):
-        ##############################
-        ###     YOUR CODE HERE     ###
-        ##############################  
-        # Define the forward pass
+        # Convolutional blocks
+        x = self.pool1(self.relu1(self.bn1(self.conv1(x))))
+        x = self.pool2(self.relu2(self.bn2(self.conv2(x))))
+        x = self.pool3(self.relu3(self.bn3(self.conv3(x))))
+        
+        # Global average pooling
+        x = self.global_pool(x)
+        
+        # Flatten
+        x = x.view(x.size(0), -1)
+        
+        # Fully connected layers with dropout
+        x = self.dropout(self.fc1_relu(self.fc1(x)))
+        x = self.dropout(self.fc2_relu(self.fc2(x)))
+        x = self.fc3(x)
         return x
