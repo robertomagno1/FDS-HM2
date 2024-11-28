6d475207811f2bb5109b6d937b2b57e288a81954
Roberto Magno Mazzotta <150812681+robertomagno1@users.noreply.github.com>
2024-11-28 17:09:29 +0100

    Update logistic_regression_penalized.py

--- libs/models/logistic_regression_penalized.py
+++ libs/models/logistic_regression_penalized.py
@@ -1,6 +1,14 @@
 import numpy as np
 from libs.models.logistic_regression import LogisticRegression
 
+import sys
+import os
+
+# Aggiungi la directory radice del progetto al PYTHONPATH
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../../..")))
+
+from libs.models.logistic_regression import LogisticRegression
+
 class LogisticRegressionPenalized(LogisticRegression):
     def __init__(self, num_features: int, lambda_: float = 0.1):
         super().__init__(num_features)
@@ -18,6 +26,11 @@ class LogisticRegressionPenalized(LogisticRegression):
             None
         """
         ##############################
-        self.parameters -= lr * (gradient - (self.lambda_ * self.parameters))
+        # Update the parameters (weights) with gradient ascent and regularization:
+        # - lr * gradient: Standard update term to maximize log-likelihood.
+        # - self.lambda_ * self.parameters: Regularization term penalizing large weights.
+        #   This helps prevent overfitting by shrinking weights towards zero.
+        self.parameters += lr * (gradient - (self.lambda_ * self.parameters))
         ##############################
         pass
+    
